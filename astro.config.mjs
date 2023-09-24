2a63a402d2056ad740481e28e4bc5df7955f6022
Ben C <bwc9876@gmail.com>
2023-09-24 13:37:03 -0400

    Astro moment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://bwc9876.github.io",
-  base: "/2023WSCSHACKATHON",
+  base: "/2023WSCSHackathon/",
   integrations: [mdx()],
 });
